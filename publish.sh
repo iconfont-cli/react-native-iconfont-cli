2bd675b2f20c99015dd9043421f3cfe81d031718
范文华 <531362022@qq.com>
2019-09-18 10:01:11 +0800

    style: Rename snapshot to snapshots

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ rm -rf ./src/iconfont
 ./node_modules/.bin/tsc
 
 mv ./build/src/* ./build
-rm -rf ./build/src ./build/snapshot
+rm -rf ./build/src ./build/snapshots
 cp README.md package.json LICENSE ./build
 cp -rf src/templates ./build/templates
 
